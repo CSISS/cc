2dc32061b07c5883f90f3b765dadc0e9eb01bc1f
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-01-11 17:41:25 -0500

    minor cleanup

--- src/main/java/edu/cyberconnector/Application.java
+++ src/main/java/edu/cyberconnector/Application.java
@@ -3,7 +3,6 @@ package edu.cyberconnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication = @Configuration | @EnableAutoConfiguration | @EnableWebMvc | @ComponentScan
 @SpringBootApplication
 public class Application {
 
