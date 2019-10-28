7a34cb1c5ab78db15a6b32fe901dbfb62389582b
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-10-28 14:45:34 -0400

    upgrade to font awesome 5.11.2

--- WebContent/covali/js/edu.gmu.csiss.covali.tools.js
+++ WebContent/covali/js/edu.gmu.csiss.covali.tools.js
@@ -14,10 +14,12 @@ edu.gmu.csiss.covali.tools = {
 			
 			var $content = "  <button type=\"button\" onclick=\"edu.gmu.csiss.covali.nco.showDialog()\" class=\"btn btn-primary\"><i class='fa fa-calculator'></i> Difference Map </button>"+
 			
-			"  <button type=\"button\" onclick=\"edu.gmu.csiss.covali.statistics.showDialog()\" class=\"btn btn-primary\"><i class='fa fa-file-excel-o'></i> Statistics Report </button>"+
+			"  <button type=\"button\" onclick=\"edu.gmu.csiss.covali.statistics.showDialog()\" class=\"btn btn-primary\"><i class='fa fa-chart-bar'></i> Statistics Report </button>"+
 			
 			"  <button type=\"button\" onclick=\"edu.gmu.csiss.covali.animation.showDialog()\" class=\"btn btn-primary\"><i class='fa fa-film'></i> Create Animation </button>"+
-			
+
+			"  <button type=\"button\" onclick=\"edu.gmu.csiss.covali.regrid.showDialog()\" class=\"btn btn-primary\"><i class='fa fa-border-none'></i> Regrid </button>"+
+
 			"  <button type=\"button\" onclick=\"edu.gmu.csiss.covali.print.showDialog()\" class=\"btn btn-primary\"><i class='fa fa-print'></i> Print </button>";
 			
 			return $content;
