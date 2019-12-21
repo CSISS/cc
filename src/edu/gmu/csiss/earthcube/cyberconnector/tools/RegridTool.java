1524ad67f0515b7064fb5b38180d287b969803ed
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-12-21 16:00:12 -0500

    regridding logging

--- src/edu/gmu/csiss/earthcube/cyberconnector/tools/RegridTool.java
+++ src/edu/gmu/csiss/earthcube/cyberconnector/tools/RegridTool.java
@@ -3,6 +3,7 @@ package edu.gmu.csiss.earthcube.cyberconnector.tools;
 import edu.gmu.csiss.earthcube.cyberconnector.utils.BaseTool;
 import edu.gmu.csiss.earthcube.cyberconnector.utils.SysDir;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 
@@ -10,12 +11,18 @@ import java.io.File;
 public class RegridTool {
 
     public static String regrid(String datafile, String gridfile, String outfile) {
+        Logger logger = Logger.getLogger(RegridTool.class);
+
         BaseTool t = new BaseTool();
 
         String pwd = t.getClassPath() + "regrid";
         String script = pwd + "/conda-regrid.sh";
 
         String[] args = {script, SysDir.anaconda_path, datafile, gridfile, outfile};
+
+        logger.info("regrid parameters: pwd=" + pwd + " script=" + script + " anaconda=" + SysDir.anaconda_path +
+                    " datafile=" + datafile + " gridfile=" + gridfile + " outfile=" + outfile);
+
         ProcessBuilder pb = new ProcessBuilder(args);
         pb.directory(new File(pwd));
         pb.redirectErrorStream(true);
