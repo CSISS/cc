e413baba5dbf98327d32f89a45927c6a3748ed69
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-01-24 13:26:04 -0500

    tools in jsframe

--- WebContent/covali/js/edu.gmu.csiss.covali.tools.js
+++ WebContent/covali/js/edu.gmu.csiss.covali.tools.js
@@ -10,44 +10,21 @@
 
 edu.gmu.csiss.covali.tools = {
 		
-		list: function(){
-			
-			var ui = "";
+		init: function() {
+            var dialogName = 'edu.gmu.csiss.covali.tools.jsframe.Tools';
+            var dialogTitle = 'Tools';
 
-			ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.nco.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-calculator'></i> Difference Map </button>";
-			ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.statistics.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-chart-bar'></i> Statistics Report </button>";
-			ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.animation.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-film'></i> Create Animation </button>";
-			ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.regrid.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-border-none'></i> Regrid </button>";
+            var ui = '<div class="modal-dialog" style="margin-left: 60px">';
+
+            ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.nco.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-calculator'></i> Difference Map </button>";
+            ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.statistics.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-chart-bar'></i> Statistics Report </button>";
+            ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.animation.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-film'></i> Create Animation </button>";
+            ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.regrid.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-border-none'></i> Regrid </button>";
             ui += "<button type=\"button\" onclick=\"edu.gmu.csiss.covali.print.showDialog()\" class=\"btn btn-primary btn-tools\"><i class='fa fa-print'></i> Print </button>";
 
-            return ui;
-			
-		},
-		
-		init: function(){
-			
-			BootstrapDialog.show({
-	            
-				message: edu.gmu.csiss.covali.tools.list(),
-	            
-				cssClass: 'dialog-vertical-center',
-				
-				title: "Tools",
-	            
-				buttons: [{
-	            
-					label: 'Close',
-	                
-					action: function(dialogItself){
-	                
-						dialogItself.close();
-	                
-					}
-	            
-				}]
-	        
-			});
-			
-		}
+            ui += '</div>';
+
+            edu.gmu.csiss.covali.menu.createDialog(dialogName, dialogTitle, ui, 160);
+        }
 		
 }
