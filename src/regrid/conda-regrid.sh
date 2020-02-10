ad7348e12f56f096fc82c9dd611a0bf3344daf24
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-02-10 10:53:42 -0500

    echo all and right order the params

--- src/regrid/conda-regrid.sh
+++ src/regrid/conda-regrid.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
+set -x
 # ex: source /opt/anaconda3/etc/profile.d/conda.sh
 
-echo "source "$2"/etc/profile.d/conda.sh"
-
-source "$2/etc/profile.d/conda.sh"
-
-echo "conda activate esmpy8"
+source "$1/etc/profile.d/conda.sh"
 
 conda activate esmpy8
-
-echo "python "$6" "$2" "$3" "$4
 
 # regrid.py datafile gridfile outfile
 python "$5" "$2" "$3" "$4"
