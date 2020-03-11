44993666945813159cd138ae60cfe91b1b7d4b83
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-03-11 13:58:51 -0400

    mac and linux installer

---
+++ install/config-conda-esmf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Conda: source "$1"/etc/profile.d/conda.sh 
+source "$1"/etc/profile.d/conda.sh 
+exit 0
+echo "Creating conda environment 'esmpy8'"
+conda create -n esmpy8 -y 
+
+echo "Installing conda xesmf and dependencies"
+conda install -y -n esmpy8  -c conda-forge "esmf!=*nompi" esmpy xarray=0.14.1 scipy dask netCDF4 xesmf libnetcdf=4.6.2 netcdf4=1.5.1
