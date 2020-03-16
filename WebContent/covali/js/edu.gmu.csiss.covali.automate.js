faa16ec12cc1c9c5bacd63d628dfdb7331dda069
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-03-16 12:09:13 -0400

    wms automation testing helper

--- WebContent/covali/js/edu.gmu.csiss.covali.automate.js
+++ WebContent/covali/js/edu.gmu.csiss.covali.automate.js
@@ -5,13 +5,38 @@ edu.gmu.csiss.covali.automate = {
             edu.gmu.csiss.covali.menu.closeAllDialogs();
             edu.gmu.csiss.covali.wms.addLayer('right', 'GFS_CONUS_80km_20170611_0000-klr2/Temperature_isobaric', "default-scalar/default", "2017-06-11T00:00:00.000Z", "100.0");
             edu.gmu.csiss.covali.wms.addLayer('left', "asr15km.anl.2D.20010101-7rfp/PSFC", "default-scalar/default", "2001-01-01T00:00:00.000Z", "");
-            setTimeout(function() {
-
-
-
-            }, 3000);
-            edu.gmu.csiss.covali.statistics.showDialog();
         }, 1000);
+    },
+
+    loadRemoteWMS: function(){
+        // edu.gmu.csiss.covali.wms.parse(null, 'http://nsidc.org/cgi-bin/acap.pl?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0');
+        edu.gmu.csiss.covali.wms.parse(null, 'https://imageserver.gisdata.mn.gov/cgi-bin/wmsll?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities');
+        setTimeout(function() {
+            // edu.gmu.csiss.covali.wms.addLayer("left", "sea_ice_concentration_01_graded_18classes", "default", "", "");
+        }, 5500);
+    },
+
+    loadDuplicateLayer: function(){
+        edu.gmu.csiss.covali.wms.parse(null, "../../ncWMS2/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0");
+        setTimeout(function() {
+            edu.gmu.csiss.covali.menu.closeAllDialogs();
+            edu.gmu.csiss.covali.wms.addLayer('left', "asr15km.anl.2D.20010101-7rfp/PSFC", "default-scalar/default", "2001-01-01T00:00:00.000Z", "");
+            // edu.gmu.csiss.covali.wms.addLayer('left', "asr15km.anl.2D.20010101-7rfp/PSFC", "contours", "2001-01-01T00:00:00.000Z", "");
+            setTimeout(function() {
+            }, 3000);
+            // edu.gmu.csiss.covali.statistics.showDialog();
+        }, 1000);
+    },
+
+    createAnimation: function() {
+        var layername = "asr15km.anl.2D.20010101-7rfp/ISLTYP";
+        var mapside = "right";
+        var starttime = "2001-01-01T00:00:00.000Z";
+        var endtime = "2001-01-01T18:00:00.000Z";
+        var framerate ="1";
+        setTimeout(function() {
+            edu.gmu.csiss.covali.wms.loadAnimation(layername, mapside, starttime, endtime, framerate);
+        }, 1400);
     },
 
     testRegrid: function() {
