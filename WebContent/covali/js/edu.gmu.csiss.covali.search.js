7e753da8103344ccaa928c7b13f15d3818d3bb45
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-04-03 14:00:22 -0400

    search dialog: formats listing improve layout

--- WebContent/covali/js/edu.gmu.csiss.covali.search.js
+++ WebContent/covali/js/edu.gmu.csiss.covali.search.js
@@ -742,8 +742,10 @@ edu.gmu.csiss.covali.search = {
 			'			</div>'+
 			
 			'			<div class="row">'+
-			
-			'				  <div class="col-md-12">'+
+
+			'				  <div class="col-md-4"></div>'+
+
+			'				  <div class="col-md-4">'+
 			
 			'						<ul class="list-unstyled" id="formatlist">'+
 			
