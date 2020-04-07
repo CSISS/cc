a927a39998436d05e4818cdba9ceecea9dc00286
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-04-07 12:00:06 -0400

    better test automation example data to test animation

--- WebContent/covali/js/edu.gmu.csiss.covali.automate.js
+++ WebContent/covali/js/edu.gmu.csiss.covali.automate.js
@@ -12,14 +12,17 @@ edu.gmu.csiss.covali.automate = {
         edu.gmu.csiss.covali.wms.parse(null, "../../ncWMS2/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0");
         setTimeout(function() {
             edu.gmu.csiss.covali.menu.closeAllDialogs();
-            edu.gmu.csiss.covali.wms.addLayer('left', 'GFS_CONUS_80km_20170611_0000-klr2/Temperature_isobaric', "default-scalar/default", "2017-06-11T00:00:00.000Z", "100.0");
-            edu.gmu.csiss.covali.wms.addLayer('left', "asr15km.anl.2D.20010101-7rfp/PSFC", "default-scalar/default", "2001-01-01T00:00:00.000Z", "");
+            edu.gmu.csiss.covali.wms.addLayer('left', 'GFS_CONUS_80km_20170611_0000-yzda/Temperature_isobaric', "default-scalar/default", "2017-06-11T00:00:00.000Z", "100.0");
+            edu.gmu.csiss.covali.wms.addLayer('left', 'GFS_CONUS_80km_20170611_0000-yzda/Pressure_surface', "default-scalar/default", "2017-06-11T00:00:00.000Z", "100.0",
+                ["2017-06-11T00:00:00.000Z", "2017-06-12T00:00:00.000Z", "2017-06-13T00:00:00.000Z", "2017-06-14T00:00:00.000Z"], ["100.0", "200.0", "300.0"]);
+            // edu.gmu.csiss.covali.wms.addLayer('left', "asr15km.anl.2D.20010101-7rfp/PSFC", "dsefault-scalar/default", "2001-01-01T00:00:00.000Z", "");
 
-            edu.gmu.csiss.covali.wms.addLayer('right', "asr15km.anl.2D.20010101-7rfp/ISLTYP", "default-scalar/default", "2001-01-01T00:00:00.000Z", "");
+            edu.gmu.csiss.covali.wms.addLayer('right', "asr15km.anl.2D.20010101-udzx/ISLTYP", "default-scalar/default", "2001-01-01T00:00:00.000Z", "");
+            edu.gmu.csiss.covali.wms.addLayer('right', "asr15km.anl.2D.20010101-udzx/PSFC", "default-scalar/default", "2001-01-01T00:00:00.000Z", "");
         }, 1000);
 
         setTimeout(function() {
-            edu.gmu.csiss.covali.settings.init();
+            // edu.gmu.csiss.covali.settings.init();
         }, 2000);
     },
 
