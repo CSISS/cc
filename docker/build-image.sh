cbacf9c105a07d16bca2c53cc4df8c3237b7075a
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-05-13 14:57:09 -0400

    docker support for h2

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -5,12 +5,16 @@ set -e
 cd ..
 mvn package
 
-# copy over warfile
+# copy over the most recent warfile
+# and rename it to CyberConnector.war
 warfile=`ls -t target/*.war | head -1`
 rm -f docker/CyberConnector.war
 cp "$warfile" docker/CyberConnector.war
 
+# copy over seed database
+cp -f db/cc.mv.db docker/cc.mv.db
+
 cd docker
 
 # build docker file
-docker build . -t csiss/cyberconnector
+docker build . -t csiss/cc
--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 8080:8080 -v $(pwd)/covaliFiles:/opt/jboss/covaliFiles -ti csiss/cyberconnector
+docker run -p 8080:8080 -v $(pwd)/covaliFiles:/opt/jboss/covaliFiles -ti csiss/cc
