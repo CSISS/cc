f8d799c927c49d556594f74be9aed2ea96d08e8e
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-05-18 14:43:35 -0400

    simpler generic RDA download script

--- utils/wget-rda/wget-rda.rb
+++ utils/wget-rda/wget-rda.rb
@@ -1,58 +1,74 @@
 require 'nokogiri'
 
 def url_download(url, fpath)
-    puts("D #{url} #{fpath}")
+    puts("Save #{url} -> #{fpath}")
     `wget -q --load-cookies rda-cookies.txt  -O #{fpath} '#{url}'`
 end
 
 def url_read(url)
-    puts("r #{url}")
+    puts("Read #{url}")
     return `wget -q --load-cookies rda-cookies.txt  -O - '#{url}'`
 end
 
 
-def get_group(gid, gname)
-    `mkdir -p #{gname}`
-    gurl = @dsurl + "/index.html?g=#{gid}"
 
-    # TODO: support multiple pages    
-    dsdoc = Nokogiri::HTML(url_read(gurl))
-    dsdoc.css('table.filelist tr td:nth-of-type(3) a:nth-of-type(1)').each do |l|
+
+def getyeargroup(year, gid, gname)
+	dir = "#{gname}/#{year}"
+	puts "Process #{dir}  g=#{year}#{gid}"
+	`mkdir -p #{dir}`
+
+	gurl = "https://rda.ucar.edu/data/ds630.1/index.html?g=#{year}#{gid}"
+	doc = Nokogiri::HTML(url_read(gurl))
+
+
+    doc.css('table.filelist tr td:nth-of-type(3) a:nth-of-type(1)').each do |l|
         dsurl = "https://rda.ucar.edu" + l['href']
-        fpath = "#{gname}/#{l.content}"
-        fpath += ".#{@add_ftype}" if @add_ftype
-        url_download(dsurl, fpath)
+        fpath = "#{dir}/#{l.content}"
+        
+        url_download(dsurl, fpath)     
     end
 end
 
-### PARAMETERSs
-@ds = "ds627.0"
-@group_name_filter = "ei.oper.an.sfc"
-@dsurl = "https://rda.ucar.edu/data/#{@ds}"
-# @add_ftype = "grib1"
 
-gdoc = Nokogiri::HTML(url_read(@dsurl + "/index.html"))
+
+years = 
+%w(
+2008
+2009
+2010
+2011
+2012
+2013
+2014
+2015
+2016
+2017
+)
 
 groups = []
-gdoc.css('table.filelist tr.flcolor2 a').each do |l|
-    # <a href="#sfol-wl-/data/ds627.0?g=19795">1979.ei.oper.an.ml</a>
-    if(l['href'].match(/\?g=(\d+)/)) then
-        gid = l['href'].match(/\?g=(\d+)/)[1]
-        gname = l.content
-        groups << [gid, gname] if gname.include?(@group_name_filter)
-    end
-end
+# ERA5 atmospheric monthly mean surface analysis [netCDF4]
+groups << ['34', 'e5.mnth.mean.an.sfc.nc']
+# ERA5 atmospheric monthly diurnal mean surface analysis [netCDF4]
+groups << ['33', 'e5.mnth.diur.an.sfc.nc']
 
 
-NUM_THREADS = 3
+
 threads = []
-NUM_THREADS.times do 
-    threads << Thread.new {
-        get_group(*groups.pop) until groups.empty?
-    }
+4.times do 
+	threads << Thread.new {
+		until years.empty?
+			year = years.pop
+			groups.each do |id, name|
+				getyeargroup(year, id, name)	
+			end
+			
+		end
+	}
 end
 
 
 threads.each {|t| t.join}
+
 
 
